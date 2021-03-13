a22a2418ba675e63b1cdebba747bee2e1c94f309
Kumar Prafull <eprafull@gmail.com>
2021-03-13 22:41:00 +0530

    key in map tag

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/counter';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Counter />,
   document.getElementById('root')
 );
 
