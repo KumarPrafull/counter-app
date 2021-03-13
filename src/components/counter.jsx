47abe92b3ac8ab136c2a7befe99026bf7b6cb62a
Kumar Prafull <eprafull@gmail.com>
2021-03-14 04:32:57 +0530

    component fin

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,15 +5,21 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement(){
-    console.log("Increment Clicked", this);
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+
+  handleIncrement=() => {
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() { 
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
