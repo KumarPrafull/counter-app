8c3c37249fcd0252f980447588fd065f9a479ec2
Kumar Prafull <eprafull@gmail.com>
2021-03-14 04:18:55 +0530

    minor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: 0,
-    };
+  state = {
+    count: 0,
+  };
 
-    handleIncrement(){
-        console.log("Increment Clicked", this);
-    }
+  handleIncrement(){
+    console.log("Increment Clicked", this);
+  }
 
-    render() { 
-        return (
-            <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-            </div>
-        );
-    }
+  render() { 
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
 
-    getBadgeClasses () {
-        let classes = "badge m-3 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses () {
+    let classes = "badge m-3 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    formatCount () {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
+  formatCount () {
+    const {count} = this.state;
+    return count === 0 ? "Zero" : count;
 
-    }
+  }
 }
  
 export default Counter;
